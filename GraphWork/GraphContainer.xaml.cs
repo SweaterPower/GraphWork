7cf07f044c4d1e1c60ef819370bcf44bbd5046a1
SweaterPower <switer.ad@gmail.com>
2019-03-11 14:04:21 +0500

    moving on

--- GraphWork/GraphContainer.xaml.cs
+++ GraphWork/GraphContainer.xaml.cs
@@ -24,16 +24,20 @@ namespace GraphWork
         {
             InitializeComponent();
             graph = new GraphWirth(1);
+            mainCanvas.SizeChanged += mainCanvas_SizeChanged;
         }
 
-        void AddVertexVisual(int value)
+        void AddVertexVisual(int key, int value)
         {
             Vertex v = new Vertex();
             v.PropertyChanged += Changed;
             mainCanvas.Children.Add(v);
-            UpdateIndexes();
-            v.Info = "Vertex:" + Environment.NewLine + (vertexes.Count).ToString();
+            //UpdateIndexes();
+            v.Info = "Vertex " + (key).ToString();
             v.Diameter = 60;
+            v.Measure(mainCanvas.RenderSize);
+            v.Arrange(new Rect(0, 0, mainCanvas.DesiredSize.Width, mainCanvas.DesiredSize.Height));
+            vertexes.Add(new Tuple<int, Vertex>(key, v));
         }
 
         void AddEdgeVisual(int from, int to, bool direct)
@@ -41,11 +45,16 @@ namespace GraphWork
             var e = (edges.Keys.FirstOrDefault((a) => { return a.Item1 == from && a.Item2 == to; }));
             if (e == null)
             {
-                EdgeContainer l = new EdgeContainer((Vertex)mainCanvas.Children[from + start - 1], (Vertex)mainCanvas.Children[to + start - 1], mainCanvas, direct);
-                mainCanvas.Children.Add(l);
-                edges.Add(new Tuple<int, int>(from, to), new Tuple<EdgeContainer, bool>(l, true));
-                edges.Add(new Tuple<int, int>(to, from), new Tuple<EdgeContainer, bool>(l, false));
-                UpdateSeparation();
+                var fromVertex = vertexes.FirstOrDefault((v) => { return v.Item1 == from; });
+                var toVertex = vertexes.FirstOrDefault((v) => { return v.Item1 == to; });
+                if (fromVertex != null && toVertex != null)
+                {
+                    EdgeContainer l = new EdgeContainer(fromVertex.Item2, toVertex.Item2, mainCanvas, direct);
+                    mainCanvas.Children.Add(l);
+                    edges.Add(new Tuple<int, int>(from, to), new Tuple<EdgeContainer, bool>(l, true));
+                    edges.Add(new Tuple<int, int>(to, from), new Tuple<EdgeContainer, bool>(l, false));
+                    UpdateSeparation();
+                }
             }
             else
             {
@@ -76,13 +85,16 @@ namespace GraphWork
 
         void DeleteVertex(int id)
         {
-            vertexes.Remove(id);
+            var toremove = vertexes.FirstOrDefault((v) => { return v.Item1 == id; });
+            if (toremove != null)
+                vertexes.Remove(toremove);
         }
 
         GraphWirth graph;
-        public ObservableCollection<int> vertexes = new ObservableCollection<int>();
-        public SortedDictionary<Tuple<int, int>, Tuple<EdgeContainer, bool>> edges = new SortedDictionary<Tuple<int, int>, Tuple<EdgeContainer, bool>>();
-        int start = 0;
+        public ObservableCollection<Tuple<int, Vertex>> vertexes = new ObservableCollection<Tuple<int, Vertex>>();
+        public Dictionary<Tuple<int, int>, Tuple<EdgeContainer, bool>> edges = new Dictionary<Tuple<int, int>, Tuple<EdgeContainer, bool>>();
+        List<int> BFSlist = new List<int>();
+        List<List<int>> levels = new List<List<int>>();
 
         public void AddVertex()
         {
@@ -113,7 +125,7 @@ namespace GraphWork
             var root = graph.root;
             while (root != null)
             {
-                AddVertexVisual(root.Value);
+                AddVertexVisual(root.Key, root.Value);
                 root = root.Next;
             }
             root = graph.root;
@@ -128,6 +140,8 @@ namespace GraphWork
                 root = root.Next;
             }
             UpdateSeparation();
+            //UpdateBFSlist();
+            UpdateVertexesLayout();
         }
 
         void UpdateSeparation()
@@ -146,34 +160,191 @@ namespace GraphWork
                     lines.Add((UIElement)child);
                 }
             }
-            Random rnd = new Random();
             mainCanvas.Children.Clear();
             foreach (var line in lines)
             {
                 mainCanvas.Children.Add(line);
             }
-            start = lines.Count;
             foreach (var point in points)
             {
                 mainCanvas.Children.Add(point);
-                var vertex = point as Vertex;
-                if (vertex != null)
-                {
-                    vertex.X = rnd.NextDouble() * (mainCanvas.ActualWidth - 50);
-                    vertex.Y = rnd.NextDouble() * (mainCanvas.ActualHeight - 50);
-                }
             }
-            UpdateIndexes();
         }
 
-        void UpdateIndexes()
+        List<int> LevelUp(ref Queue<VertexWirth> vertexes, ref List<int> visited)
         {
-            vertexes.Clear();
-            int index = 1;
-            for (int i = start; i < mainCanvas.Children.Count; i++)
+            List<int> ret = new List<int>();
+            Queue<VertexWirth> newLevel = new Queue<VertexWirth>();
+
+            while (vertexes.Count != 0)
             {
-                vertexes.Add(index);
-                index++;
+                var trail = vertexes.Dequeue().Trail;
+                while (trail != null)
+                {
+                    if (!visited.Contains(trail.Id.Key))
+                    {
+                        newLevel.Enqueue(trail.Id);
+                        ret.Add(trail.Id.Key);
+                        visited.Add(trail.Id.Key);
+                    }
+                    trail = trail.Next;
+                }
+            }
+            while (newLevel.Count != 0)
+            {
+                vertexes.Enqueue(newLevel.Dequeue());
+            }
+            return ret;
+        }
+
+        void UpdateVertexesLayout()
+        {
+            /*int count = vertexes.Count;
+            int power = 1;
+            int himan = 2;
+            int grayskull = 2;
+            while (himan < count)
+            {
+                himan *= grayskull;
+                power++;
+            }
+            int rowCount = count / power;
+            int colCount = power + count % power;//rowCount + count % power;
+            double stepX = mainCanvas.ActualWidth / colCount;
+            double stepY = mainCanvas.ActualHeight / rowCount;
+            int rowIndex = 0;
+            int colIndex = 0;
+            foreach (var key in BFSlist)
+            {
+                var vertex = vertexes.FirstOrDefault((v) => { return v.Item1 == key; }).Item2;
+                if (vertex != null)
+                {
+                    //vertex.X = rnd.NextDouble() * (mainCanvas.ActualWidth - 50);
+                    //vertex.Y = rnd.NextDouble() * (mainCanvas.ActualHeight - 50);
+                    vertex.X = (stepX * colIndex) + stepX / 4;
+                    vertex.Y = (stepY * rowIndex) + stepY / 4;
+                    colIndex++;
+                    if (colIndex >= colCount)
+                    {
+                        colIndex = 0;
+                        rowIndex++;
+                    }
+                }
+            }*/
+            foreach (var level in levels)
+                level.Clear();
+            levels.Clear();
+            var root = graph.root;
+            List<int> visited = new List<int>();
+            Queue<VertexWirth> vertexes = new Queue<VertexWirth>();
+
+            List<int> rootLevel = new List<int>();
+            rootLevel.Add(root.Key);
+            levels.Add(rootLevel);
+            vertexes.Enqueue(root);
+            visited.Add(root.Key);
+
+            while (vertexes.Count != 0)
+            {
+                var level = LevelUp(ref vertexes, ref visited);
+                levels.Add(level);
+            }
+            root = graph.root;
+            List<int> lastLevel = new List<int>();
+            while (root != null)
+            {
+                if (!visited.Contains(root.Key))
+                {
+                    var trail = root.Trail;
+                    List<int> dom = null;
+                    int batya = -1;
+                    while (trail != null)
+                    {
+                        dom = levels.FirstOrDefault((l) => { return l.Contains(trail.Id.Key); });
+                        if (dom != null)
+                        {
+                            batya = trail.Id.Key;
+                            break;
+                        }
+                        trail = trail.Next;
+                    }
+                    if (dom != null)
+                    {
+                        dom.Insert(dom.IndexOf(batya), root.Key);
+                    }
+                    else
+                    {
+                        lastLevel.Add(root.Key);
+                    }
+                }
+                root = root.Next;
+            }
+            if (lastLevel.Count != 0)
+                levels.Add(lastLevel);
+
+            bool even = true;
+            double width = mainCanvas.ActualWidth;
+            double stepY = mainCanvas.ActualHeight / levels.Count;
+            double Y = stepY / 4;
+            foreach (var level in levels)
+            {
+                double stepX = width / level.Count;
+                double X = stepX / 3 * (even ? 2 : 1);
+                foreach (var vertexKey in level)
+                {
+                    var vertexTuple = this.vertexes.FirstOrDefault((v) => { return v.Item1 == vertexKey; });
+                    if (vertexTuple != null)
+                    {
+                        var vertex = vertexTuple.Item2;
+                        vertex.X = X;
+                        vertex.Y = Y;
+                    }
+                    X += stepX;
+                }
+                Y += stepY + stepY / 4 * (even ? 2 : 1);
+                even = !even;
+            }
+        }
+
+        //void UpdateIndexes()
+        //{
+        //    vertexes.Clear();
+        //    int index = 1;
+        //    for (int i = start; i < mainCanvas.Children.Count; i++)
+        //    {
+        //        vertexes.Add(index);
+        //        index++;
+        //    }
+        //}
+
+        void UpdateBFSlist()
+        {
+            BFSlist.Clear();
+            List<int> visited = new List<int>();
+            Queue<VertexWirth> tovisit = new Queue<VertexWirth>();
+
+            VertexWirth vertex = graph.root;
+            tovisit.Enqueue(vertex);
+            while (tovisit.Count != 0)
+            {
+                vertex = tovisit.Dequeue();
+                BFSlist.Add(vertex.Key);
+                visited.Add(vertex.Key);
+                EdgeWirth trail = vertex.Trail;
+                while (trail != null)
+                {
+                    if (!visited.Contains(trail.Id.Key))
+                        tovisit.Enqueue(trail.Id);
+
+                    trail = trail.Next;
+                }
+            }
+            vertex = graph.root;
+            while (vertex != null)
+            {
+                if (!visited.Contains(vertex.Key))
+                    BFSlist.Add(vertex.Key);
+                vertex = vertex.Next;
             }
         }
 
@@ -220,6 +391,16 @@ namespace GraphWork
                         }
                         break;
                 }
+        }
+
+        private void mainCanvas_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (e.NewSize.IsEmpty || e.NewSize == new Size())
+            {
+                e.Handled = true;
+                return;
+            }
+            UpdateVertexesLayout();
         }
     }
 }
--- GraphWork/MainWindow.xaml.cs
+++ GraphWork/MainWindow.xaml.cs
@@ -26,9 +26,25 @@ namespace GraphWork
         {
             InitializeComponent();
             CBFirst.ItemsSource = graphContainer.vertexes;
+            CBFirst.DisplayMemberPath = "Item2.Info";
             CBSecond.ItemsSource = graphContainer.vertexes;
+            CBSecond.DisplayMemberPath = "Item2.Info";
             graphContainer.AddVertex(2);
             graphContainer.AddEdge(1, 2, true);
+            graphContainer.AddVertex(3);
+            graphContainer.AddEdge(2, 3, true);
+            graphContainer.AddVertex(4);
+            graphContainer.AddEdge(3, 4, true);
+            graphContainer.AddVertex(5);
+            graphContainer.AddEdge(4, 5, true);
+            graphContainer.AddVertex(6);
+            graphContainer.AddEdge(5, 6, true);
+            graphContainer.AddVertex(7);
+            graphContainer.AddEdge(3, 7, true);
+            graphContainer.AddVertex(8);
+            graphContainer.AddEdge(3, 8, true);
+            graphContainer.AddVertex(9);
+            graphContainer.AddEdge(1, 9, true);
             CBFirst.SelectedIndex = 0;
             CBSecond.SelectedIndex = 1;
         }
@@ -40,7 +56,11 @@ namespace GraphWork
 
         private void AddEdge(object sender, RoutedEventArgs e)
         {
-            graphContainer.AddEdge((int)CBFirst.SelectedItem, (int)CBSecond.SelectedItem, hasArrow.IsChecked.Value);
+            var from = CBFirst.SelectedItem as Tuple<int, Vertex>;
+            var to = CBSecond.SelectedItem as Tuple<int, Vertex>;
+            //graphContainer.AddEdge((int)CBFirst.SelectedItem, (int)CBSecond.SelectedItem, hasArrow.IsChecked.Value);
+            if (from != null && to != null)
+                graphContainer.AddEdge(from.Item1, to.Item1, hasArrow.IsChecked.Value);
             CBFirst.SelectedIndex = 0;
             CBSecond.SelectedIndex = 1;
         }
